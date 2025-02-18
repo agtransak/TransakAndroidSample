8fe8b96809dbc998302ce33c46bd5e0888301a27
Anshul Garg <anshul.garg@transak.com>
2025-02-18 17:53:30 +0530

    Add: WebView events handling using JSBridge

--- app/src/main/java/com/transak/sample/MainActivity.kt
+++ app/src/main/java/com/transak/sample/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.transak.sample
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
-import android.webkit.WebView
+import android.util.Log
+import android.webkit.JavascriptInterface
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.transak.sample.databinding.ActivityMainBinding
+import org.json.JSONObject
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private lateinit var viewbinding: ActivityMainBinding
 
-        //init webview
-        val webView: WebView = findViewById(R.id.transakWidget);
-        webView.getSettings().setJavaScriptEnabled(true)
-        webView.settings.domStorageEnabled = true
-        webView.loadUrl("url here")
+    private fun initViewBinding() {
+        viewbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewbinding.root)
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initViewBinding()
+
+        viewbinding.transakWidget.run {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            this.addJavascriptInterface(WebAppInterface(this@MainActivity), "Android")
+            loadUrl(getTransakWidgetURL())
+        }
+    }
+}
+
+private fun getTransakWidgetURL(): String {
+    return "https://global-stg.transak.com?apiKey=YOUR_API_KEY&environment=STAGING"
+}
+
+class WebAppInterface(private val context: Context) {
+    @JavascriptInterface
+    fun postMessage(eventData: String) {
+        Log.d("WebViewEvent", "postMessage: $eventData")
+        val json = JSONObject(eventData)
+        val eventId = json.getString("event_id")
+        Toast.makeText(context, eventId, Toast.LENGTH_SHORT).show()
+    }
 }
